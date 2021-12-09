ea05c4a8b0f567d26e5a8b14ae3a93f0a62d9e52
kylekrause3 <kkrause317@gmail.com>
2021-12-09 13:39:05 -0500

    Days from school laptop

---
+++ 2021-1.1/Day12021.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Day12021{
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> alist = new ArrayList<Integer>();
+		int cache = Integer.MAX_VALUE;
+		int current = 0;
+		int counter = 0;
+		try {
+			File input = new File("C:\\Users\\Kyle Krause\\eclipse-workspace\\AdventOfCode\\2021-1.1\\Input");
+			Scanner scan = new Scanner(input);
+			while(scan.hasNextLine()) {
+				alist.add(scan.nextInt());
+				/*int data = scan.nextInt();
+				if(data > cache) counter += 1;
+				cache = data;*/
+			}
+		} catch(FileNotFoundException e) {
+			System.out.println("Err");
+		}
+		
+		for(int i = 0; i < alist.size()-2; i++) {
+			current = alist.get(i) + alist.get(i+1) + alist.get(i + 2);
+			if(current > cache) counter += 1;
+			cache = current;
+		}
+		System.out.println(counter);
+	}
+}
