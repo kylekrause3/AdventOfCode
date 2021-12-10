165ac524d992a2511e8411c8281d81a72e9062b2
kylekrause3 <kkrause317@gmail.com>
2021-12-09 21:50:43 -0500

    2021 day 4 work

--- '2021-4/day42021.java
+++ '2021-4/day42021.java
@@ -1,22 +1,33 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class day42021 {
 
 	public static void main(String[] args) {
 	    try{
-	      File input = new File("./testinput42021");
+	      File input = new File("../AdventOfCode/'2021-4/TestInput2021-4");
 	      Scanner scan = new Scanner(input);
 
 	      String stuff = "";
-
-	      while(scan.hasNextLine()) stuff += scan.nextLine();
+	      
+	      String nxtln;
+	      
+	      /*while(scan.hasNextLine()) {
+	    	  stuff += scan.nextLine();
+	      }*/
+	      while(scan.hasNextLine()) {
+	    	  nxtln = scan.nextLine();
+	    	  if(nxtln.equals("")) stuff += "KYLE KRAUSE";
+	    	  else stuff += nxtln;
+	      }
+	      System.out.println("done scanning" + stuff);
 	      scan.close(); 
 
-	      String[] nums = stuff.split("\n\n");
-
+	      String[] nums = stuff.split("KYLE KRAUSE");
+	      //for (String i : nums) System.out.print(i + "(next String)");
 	      playbingo(nums);
 
 	    } catch(FileNotFoundException e){
@@ -25,72 +36,110 @@ public class day42021 {
 	  }
 
 	  public static void playbingo(String[] nums){
-	    String[] pickstr = nums[0].split(",");
-	    ArrayList<String[]>  boards = new ArrayList();
+		  System.out.println("playing bingo");
+		  String[] pickstr = nums[0].split(",");
+		  //for (String i : nums) System.out.print(i + "(next String)");
+		  System.out.println();
+		  int points;
+		  ArrayList<ArrayList<String>>  boards = new ArrayList<ArrayList<String>>();
 
-	    for(int i = 1; i > nums.length; i++) boards.add(nums[i].split(" "));
-
-	    for(int i = 0; i < pickstr.length; i++){
-	      for(int j = 0; j < boards.size(); j++) checkboard(boards.get(j), pickstr[i]);
-	    }
+		  for(int i = 1; i < nums.length; i++) boards.add(new ArrayList<String>(Arrays.asList(nums[i].split(" "))));
+		  for(int i = 0; i < boards.size(); i++) {
+			  for(int j = 0; j < boards.get(i).size();) {
+				  if(boards.get(i).get(j).equals("")) {
+					  boards.get(i).remove(j);
+				  }
+				  else j++;
+			  }
+		  }
+		  ArrayList<ArrayList<String>>  cachedboards = new ArrayList<ArrayList<String>>(boards);
+		  
+		  for(int i = 0; i < pickstr.length; i++){
+			  for(int j = 0; j < boards.size(); j++) {
+				  points = checkboard(boards.get(j), cachedboards.get(j), pickstr[i]);
+				  if(points != 0) System.out.println(points);
+			  }
+		  }
 	  }
 
-	  public static void checkboard(String[] board, String numstr){
-	    ArrayList<String> winner = new ArrayList();
-	    boolean addedone = false;
-	    for(int i = 0; i < board.length; i++){
-	      if(board[i].equals(numstr)){
-	        board[i] = "x";
-	        addedone = true;
-	        break;
-	      }
-	    }
-
-	    int[][] counter = new int[2][5];
-
-	    if(addedone){
-	      winner = checkrowsandcols(board);
-	      if(winner != null);
-	    }
-
+	  public static int checkboard(ArrayList<String> board, ArrayList<String> cboard, String numstr){
+		  //System.out.println("Checking board");
+		  ArrayList<String> winner = new ArrayList<String>();
+	    
+		  boolean addedone = false;
+		  for(int i = 0; i < board.size(); i++){
+			  if(board.get(i).equals(numstr)){
+				  board.set(i, "x");
+				  addedone = true;
+				  break;
+	      		}
+		  }
+	    
+		  int ret = 0;
+	    
+		  if(addedone){
+			  winner = checkrowsandcols(board, cboard);
+			  if(!winner.get(0).equals("a")) {
+				  //System.out.println(winner);
+				  for(int i = 0; i < winner.size(); i++) ret+=Integer.parseInt(winner.get(i));
+				  ret *= Integer.parseInt(numstr);
+			  }
+		  }
+	    
+		  return ret;
 	  }
 
-	  public static ArrayList<String> checkrowsandcols(String[] board){
-	    int columnind = 0;
-	    int rowind = 0;
-	    boolean colfull;
-	    boolean rowfull;
-	    ArrayList<String> col = new ArrayList();
-	    ArrayList<String> row = new ArrayList();
+	  public static ArrayList<String> checkrowsandcols(ArrayList<String> board, ArrayList<String> cboard){
+		  int columnind = 0;
+		  int rowind = 0;
+		  boolean colfull;
+		  boolean rowfull;
+		  ArrayList<String> col = new ArrayList<String>();
+		  ArrayList<String> row = new ArrayList<String>();
 
-	    for(int i = 0; i < board.length; i++){
-	      if(board[i].equals("x")) {
-	        // to get column do i % 5
-	        // to get row do (i * 2) / 10 
-	        columnind = i % 5;
-	        rowind = (int)((i * 2) / 10);
-	        for(int j = 0; j < 5; j++){
-	          col.add(board[columnind * j]);
-	          row.add(board[rowind + j]);
-	        }
-	        colfull = isAllSame(col);
-	        rowfull = isAllSame(row);
-	        if(colfull) return col;
-	        else if(rowfull) return row;
+		  for(int i = 0; i < board.size(); i++){
+			  if(board.get(i).equals("x")) {
+				  // to get column do i % 5
+				  // to get row do (i * 2) / 10 
+				  columnind = i % 5;
+				  rowind = (int)((i * 2) / 10);
+				  for(int j = 0; j < 5; j++){
+					  col.add(board.get(columnind * j));
+					  row.add(board.get(rowind + j));
+				  }
+				  System.out.println(col + " " +  row);
+				  colfull = isAllSame(col);
+				  rowfull = isAllSame(row);
+				  if(colfull) {
+					  col.clear();
+					  for(int j = 0; j < 5; j++) {
+						  col.add(cboard.get(columnind * j));
+					  }
+					  return col;
+				  }
+				  else if(rowfull) {
+					  row.clear();
+					  for(int j = 0; j < 5; j++) {
+						  row.add(cboard.get(rowind + j));
+					  }
+					  return row;
+				  }
 
-	      }
-	      col.clear();
-	      row.clear();
-	    }
-	    return null;
+			  }
+			  col.clear();
+			  row.clear();
+		  }
+		  ArrayList<String> ret = new ArrayList<String>();
+		  ret.add("a");
+		  return ret;
 	  }
 
 	  public static boolean isAllSame(ArrayList<?> list){
-	    Object first=list.get(0);
-	    for(int i=1;i<list.size();i++){
-	      if(!list.get(i).equals(first))return false;
-	    }
-	    return true;
+		  Object first=list.get(0);
+		  for(int i=1;i<list.size();i++){
+			  if(!list.get(i).equals(first))return false;
+		  }
+		  return true;
 	  }
 
 }
