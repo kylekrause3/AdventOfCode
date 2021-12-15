8d12dd63c780ce767f4abc33d4b7b71502ff5d74
kylekrause3 <kkrause317@gmail.com>
2021-12-15 14:00:24 -0500

    Update day52021.java

--- '2021-5/day52021.java
+++ '2021-5/day52021.java
@@ -17,11 +17,53 @@ public class day52021 {
 			while(scan.hasNextLine()) {
 				cords.add(scan.nextLine().split(" -> "));
 			}
-			
+			generate(cords);
 		} 
 		catch(FileNotFoundException e) {
 			System.out.println(e);
 		}
+	}
+	
+	
+	public static void generate(ArrayList<String[]> cords){
+		ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+		int x1, y1, x2, y2;
+		
+		for(int i = 0; i < cords.size(); i++) {
+			x1 = Integer.parseInt(cords.get(i)[0].split(",", 2)[0]);
+			y1 = Integer.parseInt(cords.get(i)[0].split(",", 2)[1]);
+			x2 = Integer.parseInt(cords.get(i)[1].split(",", 2)[0]);
+			y2 = Integer.parseInt(cords.get(i)[1].split(",", 2)[1]);
+			
+			int[] max = new int[] {bigger(x1, x2), bigger(y1, y2)};
+			
+			
+			int xadded = 0;
+			if(x1 == x2 || y1 == y2) {//if the line is vertical or it is horizontal(part 1)
+				if(graph.get(0).size()/*size of x axis*/ < max[0]) {
+					for(int j = 0; j < max[0]; j++) {
+						graph.get(0).add(0);
+						xadded += 1;
+					}
+					for(int j = 0; j < graph.size(); j++) {
+						for(int k = 0; k < xadded; k++) {
+							graph.get(j).add(0);
+						}
+					}
+					xadded = 0;
+				}
+				if(graph.size()/*size of y axis*/ < max[1]) {
+					
+				}
+			}
+			
+		}
+		
+	}
+	
+	public static int bigger(int a, int b) {
+		if(a > b) return a;
+		else return b;
 	}
 
 }
